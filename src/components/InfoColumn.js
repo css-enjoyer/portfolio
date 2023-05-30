033c31a3261f7ae78e64f628a6f946f4557a53f7
Isaac Reyes <isaacjdjreyes@gmail.com>
2023-05-30 17:47:34 +0800

    Style about me

--- src/components/InfoColumn.js
+++ src/components/InfoColumn.js
@@ -2,9 +2,13 @@
 function InfoColumn() {
   	return (
 	<div className="infoColumn">
-        <p>As a child, I've always enjoyed tinkering on my computer. Whether it was cheating infinite money on video games, installing those dubious custom cursors (and nearly destroying my os), or trying to run my own minecraft servers were some of the experiences that developed my problem solving skills.
-
-        Today my passion lies in web development. Creating new experiences, implementing eye pleasing designs, and making them come to life with animations is where I find fulfilment.</p>
+        <p>As a child, I've always enjoyed tinkering on my computer. Whether it was cheating infinite money on video games, installing those suspicious custom cursors (and nearly destroying my os), or trying to run my own minecraft servers were some of the experiences that developed my problem solving skills.
+        <br />
+        <br />
+        Today my passion lies in web development. Creating new experiences, implementing eye pleasing designs, and making them come to life with animations is where I find fulfilment.
+        <br />
+        <br />
+        In my free time, you can find me enjoying pool, working out, or missing my hooks in dota 2. </p>
 	</div>
   	);
 }
