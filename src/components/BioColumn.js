59eec144b3690f35c3bf8513382695f6bb81f26f
Isaac Reyes <isaacjdjreyes@gmail.com>
2023-05-30 15:46:01 +0800

    Add basic html content for bioCol

--- src/components/BioColumn.js
+++ src/components/BioColumn.js
@@ -2,7 +2,17 @@
 function BioColumn() {
   	return (
 	<div className="bioColumn">
+        <h1>Hi, my name is Isaac Reyes</h1>
+        <h4>I'm a Computer Science student at the Univeristy of Santo Tomas, Philippines</h4>
+        <p>I like to create visually pleasing websites and learning new technologies</p>
 
+        <a>About</a>
+        <a>Experience</a>
+        <a>Projects</a>
+
+        <button>GitHub</button>
+        <button>IG</button>
+        <button>LinkedIn</button>
 	</div>
   	);
 }
