ca56022a63b9784f0d275dc406b3bc20cb9eb272
Isaac Reyes <isaacjdjreyes@gmail.com>
2023-05-31 18:50:54 +0800

    Style bio column

--- src/components/BioColumn.js
+++ src/components/BioColumn.js
@@ -1,3 +1,6 @@
+import igImage from "../images/instagram.svg";
+import ghImage from "../images/github.svg";
+import liImage from "../images/linkedin.svg";
 
 function BioColumn() {
   	return (
@@ -5,16 +8,16 @@ function BioColumn() {
 		<h1>Hi, my name is <span>Isaac Reyes</span></h1>
 		<h4>I study <span>Computer Science</span> at the <br />Univeristy of Santo Tomas, Philippines</h4>
 		
-		<div className="list">
+		<div className="sec-list">
 			<a href="">About Me</a>
 			<a href="">Projects</a>
 			<a href="">Education</a>
 		</div>
-
-		<div className="list">
-			<button>GitHub</button>
-			<button>IG</button>
-			<button>LinkedIn</button>
+		
+		<div className="soc-list">
+			<button><img src={ghImage}></img></button>
+			<button><img src={igImage}></img></button>
+			<button><img src={liImage}></img></button>
 		</div>
 	</div>
   	);
