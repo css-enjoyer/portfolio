fb27c2ecfb27b74828a8bae7928649e9e72aac64
Isaac Reyes <isaacjdjreyes@gmail.com>
2023-05-31 13:02:20 +0800

    Add gradient

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
 import BioColumn from './components/BioColumn';
 import InfoColumn from './components/InfoColumn';
+import React, { useEffect, useRef } from 'react';
+
 
 function App() {
+	const bgRef = useRef(null);
+	useEffect(() => {
+		const updateMousePosition = (ev) => {
+			if(!bgRef.current) return;
+			const {clientX, clientY} = ev;
+			// console.log(clientX, clientY);
+			bgRef.current.style.setProperty("--x", `${clientX}px`);
+			bgRef.current.style.setProperty("--y", `${clientY}px`);
+		}
+		window.addEventListener('mousemove', updateMousePosition)
+		return () => {
+			window.removeEventListener('mousemove', updateMousePosition);
+		}
+	}, []) 
+	
   	return (
-	<div className="App">
+	<div ref={bgRef} className="App">
 		<BioColumn />
 		<InfoColumn />
 	</div>
