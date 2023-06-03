aa6ee6f8111c2394f08344e9a8e179763817f1af
Isaac Reyes <isaacjdjreyes@gmail.com>
2023-06-03 18:53:20 +0800

    Fix intro alignment

--- src/components/BioColumn.js
+++ src/components/BioColumn.js
@@ -5,7 +5,7 @@ import liImage from "../images/linkedin.svg";
 function BioColumn() {
   	return (
 	<div className="bioColumn">
-		<h1>Hi, my name is <span>Isaac Reyes</span></h1>
+		<h1>Hey, my name is <span>Isaac Reyes</span></h1>
 		<h4>I study <span>Computer Science</span> at the <br />Univeristy of Santo Tomas, Philippines</h4>
 		
 		<div className="sec-list">
